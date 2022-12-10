fdbaf36d2573545fbef958197e1b4bba6d8f85f0
Christian Aquino <cjfaquino@gmail.com>
2022-12-09 22:30:06 -0800

    remove old comments & placeholder

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,9 @@ const Card = (props) => {
   const { clickPokemon, object, score } = props;
   const { currentScore } = score;
 
-  // const [clicked, setClicked] = useState(false);
-
   useEffect(() => {
     // handle initialize & score resets
     if (currentScore === 0) {
-      // setClicked(false);
       object.clicked = false;
     }
   }, [currentScore]);
@@ -17,7 +14,6 @@ const Card = (props) => {
   const handleScore = () => {
     if (!object.clicked) {
       score.addCurrentScore();
-      // setClicked(true);
       clickPokemon(object.id);
 
       object.clicked = true;
@@ -34,10 +30,6 @@ const Card = (props) => {
     <div key={object.id} style={styles} onClick={handleScore}>
       <img src={object.img_url} alt={object.name} />
       <div>{object.name}</div>
-
-      {/* remove after */}
-
-      {object.clicked && <div>Clicked</div>}
     </div>
   );
 };
