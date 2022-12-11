fc29abc0fe98f419db9f9f0ea4f6938621804abd
Christian Aquino <cjfaquino@gmail.com>
2022-12-10 19:45:23 -0800

    fix mobile styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import MyFooter from './components/MyFooter/MyFooter';
 
+import './components/utils/appHeight';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
